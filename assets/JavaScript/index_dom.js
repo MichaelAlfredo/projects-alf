f3c4c9cdde6a94fb2f89e9bd9f5d39472fb278c4
MichaelAlfredo <114528249+MichaelAlfredo@users.noreply.github.com>
2023-02-23 11:01:39 -0500

    map2

--- assets/JavaScript/index_dom.js
+++ assets/JavaScript/index_dom.js
@@ -32,7 +32,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   responsiveMedia(
     "gmaps",
     "(min-width:1024px)",
-    `<a href="https://goo.gl/maps/p2Lz6gyWWoH9DFAn9" target="_blank" rel="noopener">Ver Mapa</a>`,
+    `<a href="https://goo.gl/maps/w42JFvonHS7WygkL7" target="_blank" rel="noopener">Ver Mapa</a>`,
     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25656.786708181306!2d-79.55142158437502!3d8.98296430000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8faca8e31fbb2223%3A0x2fd43fc2ee8c3d18!2sF%26F%20Tower!5e1!3m2!1ses-419!2spa!4v1677167762180!5m2!1ses-419!2spa" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
   );
 
